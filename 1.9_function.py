c8b67199dfd6773c4185c63b21cd43d7f2e449b8
cactiglitch <84656334+cactiglitch@users.noreply.github.com>
2021-06-05 22:59:43 +0800

    Update 1.9_function.py

--- 1.9_function.py
+++ 1.9_function.py
@@ -1,26 +1,6 @@
-def cheese_and_crackers(cheese_count, boxes_of_crackers):
-    print(f"You have {cheese_count} cheeses!")
-    print(f"You have {boxes_of_crackers} boxes of crackers!")
-    print("Man that's enough for a party!")
-    print("Get a blanket.\n")
-    
-print("We can just give the function numbers directly:")
-cheese_and_crackers(20,30)
-
-print("OR, we can use variables from our script:")
-amount_of_cheese = 10
-amount_of_crackers = 50
-
-cheese_and_crackers(amount_of_cheese, amount_of_crackers)
-
-print("We can do math inside too:")
-cheese_and_crackers(10 + 20, 5 + 6)
-print("And we can combine the two, variables and math:")
-cheese_and_crackers(amount_of_cheese+100, amount_of_crackers+1000)
-
 """
 You have 20 cheeses!
-You hvae 30 boxes of crackers!
+You have 30 boxes of crackers!
 Man that's enough for a party!
 Get a blanket.
 
@@ -42,3 +22,23 @@ You have 1050 boxes of crackers!
 Man that's enough for a party!
 Get a blanket.
 """
+
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print(f"You have {cheese_count} cheeses!")
+    print(f"You have {boxes_of_crackers} boxes of crackers!")
+    print("Man that's enough for a party!")
+    print("Get a blanket.\n")
+    
+print("We can just give the function numbers directly:")
+cheese_and_crackers(20,30)
+
+print("OR, we can use variables from our script:")
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+print("We can do math inside too:")
+cheese_and_crackers(10 + 20, 5 + 6)
+print("And we can combine the two, variables and math:")
+cheese_and_crackers(amount_of_cheese+100, amount_of_crackers+1000)

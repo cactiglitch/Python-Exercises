a3fa8ea51039e264e8dfe12d8d5897807ddb6b17
cactiglitch <84656334+cactiglitch@users.noreply.github.com>
2021-06-05 21:35:57 +0800

    Create 1.2_format.py

---
+++ 1.2_format.py
@@ -0,0 +1,26 @@
+"""
+Let's talk about Will SMith.
+He's 74 inches tall.
+He's 180 pounds heavy.
+He's got Blue eyes and Brown hair.
+His teeth are usually White depending on the coffee.
+If I add 35, 74, and 180, I get 289.
+"""
+
+name = 'Will SMith'
+age = 35
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+total = age + height + weight
+
+print(f"If I add {age}, {height}, and {weight}, I get {total}.")

c0c6295dbdcd6e09a57399a59a48c9a1c0fce335
cactiglitch <84656334+cactiglitch@users.noreply.github.com>
2021-06-05 23:21:57 +0800

    Create 2.0_morefunc.py

---
+++ 2.0_morefunc.py
@@ -0,0 +1,34 @@
+"""
+Let's do the math!
+ADDING 30 + 5
+SUBTRACTING 78 - 4
+MULTIPLYING 90 * 2
+DIVIDING 100 / 2
+Age: 35, Height: 74, Weight: 180, IQ: 50.0
+
+"""
+
+def add(a, b):
+    print(f"ADDING {a} + {b}")
+    return a + b
+
+def subtract(a, b):
+    print(f"SUBTRACTING {a} - {b}")
+    return a - b
+
+def multiply(a, b):
+    print(f"MULTIPLYING {a} * {b}")
+    return a * b
+
+def divide(a, b):
+    print(f"DIVIDING {a} / {b}")
+    return a / b
+
+print("Let's do the math!")
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90,2)
+iq = divide(100,2)
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
